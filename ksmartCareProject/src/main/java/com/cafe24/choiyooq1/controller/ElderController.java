0aa7e0d1e3c6f0a970a10aded0b208f3a026d719
donghachoi <cdh.m777@gmail.com>
2020-03-10 15:10:37 +0900

    [수급자 상세정보 하는중]

--- ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/ElderController.java
+++ ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/ElderController.java
@@ -1,18 +1,22 @@
 package com.cafe24.choiyooq1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cafe24.choiyooq1.mapper.BenefitMapper;
 
 @Controller
 public class ElderController {
 	
-	
+	@Autowired BenefitMapper benefitMapper;
 	
 	
 	/* 수급자 리스트 */
 	@GetMapping("/elderList")
-	public String getdelderList() {
-		
+	public String getdelderList(Model model) {
+		model.addAttribute("benefitcost", benefitMapper.getBenefitCost());
 		return "elder/elderList";
 	}
 }
