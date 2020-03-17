83b67d77b9cac7a5e3c931df49c2c1c8a3a4a4ac
donghachoi <cdh.m777@gmail.com>
2020-03-17 17:23:23 +0900

    [17 배포전] 다시

--- ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/MainController.java
+++ ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/MainController.java
@@ -7,27 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.cafe24.choiyooq1.service.LoginService;
 
 
 
 @Controller
 public class MainController {
-	
-	@Autowired
-	private LoginService loginService;
-	
-	//로그인 확인 
-	@PostMapping("/login")
-	public String login(@RequestParam(value="centerCode")String centerCode
-						,@RequestParam(value="centerId")String centerId
-						,@RequestParam(value="centerPw")String centerPw) {
-		System.out.println(loginService.checkLoginCenter(centerCode, centerId, centerPw));
-		
-		
-		return "login/firstlogin";
-	}
-	
+	/*
+	 * @Autowired private LoginService loginService;
+	 * 
+	 * //로그인 확인
+	 * 
+	 * @PostMapping("/login") public String
+	 * login(@RequestParam(value="centerCode")String centerCode
+	 * ,@RequestParam(value="centerId")String centerId
+	 * ,@RequestParam(value="centerPw")String centerPw) {
+	 * System.out.println(loginService.checkLoginCenter(centerCode, centerId,
+	 * centerPw));
+	 * 
+	 * 
+	 * return "login/firstlogin"; }
+	 */
 	
 	@GetMapping("/")
 	public String index1() {
