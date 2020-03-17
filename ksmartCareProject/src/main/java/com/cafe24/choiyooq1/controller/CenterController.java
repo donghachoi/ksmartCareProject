916c698b850891adaeca03b4127a7f9a2b5494c7
coolpulib <rambambi31@gamil.com>
2020-03-17 13:59:05 +0900

    센터리스트 백엔드 작업중

--- ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/CenterController.java
+++ ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/CenterController.java
@@ -1,14 +1,28 @@
 package com.cafe24.choiyooq1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cafe24.choiyooq1.domain.Center;
+import com.cafe24.choiyooq1.service.CenterService;
 
 @Controller
 public class CenterController {
 	
-	@GetMapping("/insertcenter")	
-	public String insertEmployee() {
-		return "center/insertCenter";
+	@Autowired CenterService centerService;
+	
+	//센터가입 폼 불러오기
+	@GetMapping("/center/centerInsert")	
+	public String centerInsert() {
+		return "center/centerInsert";
+	}
+	//센터가입 진행
+	@PostMapping("/center/centerInsert")
+	public String centerInsert(Center center) {
+		System.out.println(center.toString());
+		return null;
 	}
 	
 	@GetMapping("/centerlist")
---
+++ ksmartCareProject/src/main/java/com/cafe24/choiyooq1/service/CenterService.java
@@ -0,0 +1,5 @@
+package com.cafe24.choiyooq1.service;
+
+public class CenterService {
+
+}
