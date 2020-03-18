ddaa16fb1b5dfc12af86e9f87389fe346a7fd54f
donghachoi <cdh.m777@gmail.com>
2020-03-18 16:18:43 +0900

    [200318 로그인 처리중...]

--- ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/MainController.java
+++ ksmartCareProject/src/main/java/com/cafe24/choiyooq1/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cafe24.choiyooq1.domain.Center;
 import com.cafe24.choiyooq1.service.LoginService;
@@ -23,13 +24,23 @@ public class MainController {
 	 @Autowired private LoginService loginService;
 	 Center center = new Center();
 	  
+	 
+	 //로그아웃 
+	 @GetMapping("/logout")
+	 public String login(HttpSession session) {
+		 session.invalidate();
+		 return "login/firstlogin";
+	 }
+	 
 	 //로그인 확인
 	 @PostMapping("/login") 
 	 public String login(@RequestParam(value="centerCode")String centerCode
 						 ,@RequestParam(value="centerId")String centerId
 						 ,@RequestParam(value="centerPw")String centerPw
 						 ,HttpServletRequest request
+						 ,RedirectAttributes redirectA
 						 ,Model model) {
+		 
 		 center = loginService.checkLoginCenter(centerCode, centerId, centerPw);
 		 HttpSession session = request.getSession();
 		 
@@ -41,15 +52,12 @@ public class MainController {
 			 model.addAttribute("SID", center.getCenterId());
 			 model.addAttribute("SCENTERNAME", center.getCenterName());
 			 model.addAttribute("SMANAGERNAME", center.getCenterManagerName());
+			 return "index1"; 
+
 		 }else {
-			 
-			 session.setAttribute("SID", null);
-			 session.setAttribute("SCENTERCODE", null);
-			 System.out.println(session.getAttribute("SID"));
-			 System.out.println(session.getAttribute("SCENTERCODE"));
+			 redirectA.addAttribute("result", "등록된 회원이 아닙니다.");	
+			return "redirect:/login";
 		 }
-		 return "index1"; 
-		 
 	  }
 
 	@GetMapping("/")
@@ -63,8 +71,11 @@ public class MainController {
 		return "index";
 	}
 	
+	//로그인
 	@GetMapping("/login")
-	public String firstlogin() {
+	public String firstlogin(@RequestParam(value="result", required = false) String result
+			, Model model) {
+		model.addAttribute("result", result);
 		return "login/firstlogin";
 	}
 	
