0d0145e62e723175171cb3919008c6c66b503ecd
coolpulib <rambambi31@gamil.com>
2020-03-27 11:44:57 +0900

    보호자 수정, 삭제 완료

--- ksmartCareProject/src/main/java/com/cafe24/choiyooq1/mapper/GuardianMapper.java
+++ ksmartCareProject/src/main/java/com/cafe24/choiyooq1/mapper/GuardianMapper.java
@@ -14,4 +14,14 @@ public interface GuardianMapper {
 	
 	//보호자 리스트
 	public List<Guardian> guardianList();
+	
+	//보호자 정보수정을 위한 불러오기
+	public Guardian guardianSelectForUpdate(String guardianId);
+	
+	//보호자 정보수정
+	public int guardianUpdate(Guardian guardian);
+	
+	//보호자 삭제
+	public int guardianDelte(String guardianId);
+	
 }
